d812cbe3e72379bec822b068a62734d309350d93
Ines BEUGRE <ines.beugre@niji.fr>
2020-04-26 19:32:02 +0200

    fix: remove @fortawesome/fontawesome-free/css/all.min.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import thunk from 'redux-thunk'
 import {applyMiddleware, createStore, combineReducers} from 'redux';
 import formationsReducer from './redux/formations/reducers';
 import filtersReducer from './redux/filters/reducers';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
 import Main from './main';
 
 const reducer = combineReducers({
